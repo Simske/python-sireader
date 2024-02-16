1e16ecd826b81c9d218fb1cbc2ee9ea3d9d1b994
Simske <mail@simske.com>
2024-02-16 22:03:14 +0100

    refactor: restructure repository and introduce poetry for dependency management

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+TODO: Readme has to be updated
+
 # sportident-python
 
 Python code for talking to Sportident stations. Used e.g. in the sport of orienteering.
---
+++ sireader/__init__.py
@@ -0,0 +1,9 @@
+from .sireader2 import SIReader, SIReaderControl, SIReaderException, SIReaderReadout
+
+__all__ = [
+    "SIReader",
+    "SIReaderControl",
+    "SIReaderReadout",
+    "SIReaderException",
+    "byte2int",
+]
